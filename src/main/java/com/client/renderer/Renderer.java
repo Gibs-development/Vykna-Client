d9e6066d5f342b815dccdc4a943a5f8c88505dfa
Gibs-development <cg.lizun@gmail.com>
2026-01-13 10:56:45 +0000

    GPU Cleanup

--- src/main/java/com/client/renderer/Renderer.java
+++
@@ -1,9 +0,0 @@
-package com.client.renderer;
-
-public interface Renderer {
-    void drawFrame(RenderContext context);
-
-    void resize(int width, int height);
-
-    void shutdown();
-}
--- src/main/java/com/client/renderer/RendererMode.java
+++
@@ -1,7 +0,0 @@
-package com.client.renderer;
-
-public enum RendererMode {
-    SOFTWARE,
-    GPU_PRESENTER,
-    GPU_RENDERER
-}
