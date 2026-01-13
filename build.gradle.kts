b70bd28e2a5d2b9890ea66cb82129f60de3a7748
Gibs-development <cg.lizun@gmail.com>
2026-01-13 14:22:58 +0000

    Mirror RuneLite Gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,125 +1,166 @@
-import proguard.gradle.ProGuardTask
-import org.gradle.jvm.tasks.Jar
+import com.github.jengelman.gradle.plugins.shadow.ShadowApplicationPlugin.SHADOW_INSTALL_TASK_NAME
+import com.github.jengelman.gradle.plugins.shadow.ShadowApplicationPlugin.SHADOW_SCRIPTS_TASK_NAME
 
 plugins {
     java
+    application
+    id("com.github.johnrengelman.shadow")
+}
+
+group = "com.osroyale"
+version = "1.0"
+
+dependencies {
+    annotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.20")
+    annotationProcessor(group = "org.pf4j", name = "pf4j", version = "3.6.0")
+
+    compileOnly(group = "javax.annotation", name = "javax.annotation-api", version = "1.3.2")
+    compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.20")
+    compileOnly(group = "net.runelite", name = "orange-extensions", version = "1.0")
+
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.9")
+    implementation(group = "com.google.code.gson", name = "gson", version = "2.8.5")
+    implementation(group = "com.google.guava", name = "guava", version = "30.1.1-jre") {
+        exclude(group = "com.google.code.findbugs", module = "jsr305")
+        exclude(group = "com.google.errorprone", module = "error_prone_annotations")
+        exclude(group = "com.google.j2objc", module = "j2objc-annotations")
+        exclude(group = "org.codehaus.mojo", module = "animal-sniffer-annotations")
+    }
+    implementation(group = "com.google.inject", name = "guice", version = "5.0.1")
+    implementation(group = "com.jakewharton.rxrelay3", name = "rxrelay", version = "3.0.1")
+    implementation(group = "com.squareup.okhttp3", name = "okhttp", version = "4.9.1")
+    implementation(group = "io.reactivex.rxjava3", name = "rxjava", version = "3.1.2")
+    implementation(group = "org.jgroups", name = "jgroups", version = "5.2.2.Final")
+    implementation(group = "net.java.dev.jna", name = "jna", version = "5.9.0")
+    implementation(group = "net.java.dev.jna", name = "jna-platform", version = "5.9.0")
+    implementation(group = "net.runelite", name = "discord", version = "1.4")
+    implementation(group = "net.runelite.pushingpixels", name = "substance", version = "8.0.02")
+    implementation(group = "net.sf.jopt-simple", name = "jopt-simple", version = "5.0.4")
+    implementation(group = "org.madlonkay", name = "desktopsupport", version = "0.6.0")
+    implementation(group = "org.apache.commons", name = "commons-text", version = "1.9")
+    implementation(group = "org.apache.commons", name = "commons-csv", version = "1.9.0")
+    implementation(group = "commons-io", name = "commons-io", version = "2.8.0")
+    implementation(group = "org.jetbrains", name = "annotations", version = "22.0.0")
+    implementation(group = "com.github.zafarkhaja", name = "java-semver", version = "0.9.0")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.32")
+    implementation(group = "org.pf4j", name = "pf4j", version = "3.6.0") {
+        exclude(group = "org.slf4j")
+    }
+
+    implementation(group = "net.runelite.jogl", name = "jogl-rl", version = "2.4.0-rc-20220318")
+    implementation(group = "net.runelite.jogl", name = "jogl-gldesktop-dbg", version = "2.4.0-rc-20220318")
+    implementation(group = "net.runelite.jocl", name = "jocl", version = "1.0")
+
+    runtimeOnly(group = "net.runelite.pushingpixels", name = "trident", version = "1.5.00")
+
+    testAnnotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.20")
+
+    testCompileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.20")
+
+    testImplementation(group = "com.google.inject.extensions", name = "guice-grapher", version = "4.1.0")
+    testImplementation(group = "com.google.inject.extensions", name = "guice-testlib", version = "4.1.0")
+    testImplementation(group = "org.hamcrest", name = "hamcrest-library", version = "1.3")
+    testImplementation(group = "junit", name = "junit", version = "4.13.1")
+    testImplementation(group = "org.mockito", name = "mockito-core", version = "3.1.0")
+    testImplementation(group = "org.mockito", name = "mockito-inline", version = "3.1.0")
+    testImplementation(group = "com.squareup.okhttp3", name = "mockwebserver", version = "4.9.1")
+    testImplementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.32")
+    implementation("io.sentry:sentry-logback:6.0.0")
+
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.1"))
+    implementation("org.lwjgl:lwjgl")
+    implementation("org.lwjgl:lwjgl-opengl")
+    implementation(group = "org.lwjgl", name = "lwjgl-opencl")
+    implementation("net.runelite:rlawt:1.3")
+
+    listOf("linux", "macos", "macos-arm64", "windows-x86", "windows").forEach {
+        runtimeOnly("org.lwjgl:lwjgl::natives-$it")
+        runtimeOnly("org.lwjgl:lwjgl-opengl::natives-$it")
+    }
+
+    implementation(files("libs/SwiftFUP-client-1.9.1.jar"))
+    implementation(files("libs/allatori-annotations.jar"))
+    implementation("it.unimi.dsi:fastutil:8.5.12")
+    implementation("club.minnced:java-discord-rpc:2.0.2")
+    implementation("com.displee:rs-cache-library:6.9")
+    implementation("com.thoughtworks.xstream:xstream:1.4.7")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+    implementation("com.dorkbox:Notify:3.7")
+    implementation("net.oneandone.reflections8:reflections8:0.11.7")
+
+    val netty = "4.1.96.Final"
+    implementation("io.netty:netty-handler:$netty")
+    implementation("io.netty:netty-transport-native-epoll:$netty")
+    implementation("io.netty:netty-transport-native-kqueue:$netty")
+}
+
+application {
+    mainClass.set("com.osroyale.Client")
+    applicationDefaultJvmArgs += arrayOf(
+        "-XX:-OmitStackTraceInFastThrow",
+        "-Xmx2g",
+        "-Xms1g"
+    )
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("-Xlint:none")
-    options.isFork = true
-}
+tasks {
+    shadowJar {
+        archiveBaseName.set("Tarnish")
+        archiveClassifier.set("")
+        archiveVersion.set("")
 
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://repo.runelite.net")
-}
-
-buildscript {
-    repositories {
-        mavenCentral()
+        isZip64 = true
     }
-    dependencies {
-        classpath("net.sf.proguard:proguard-gradle:6.0.2")
+    named<Zip>("distZip").configure {
+        enabled = false
     }
-}
-
-sourceSets {
-    named("main") {
-        java.srcDirs(
-            "src/main/java",
-        )
-        resources.srcDirs(
-            "runelite/http-api/src/main/resources",
-            "runelite/runelite-client/src/main/resources"
-        )
+    named<Tar>("distTar").configure {
+        enabled = false
     }
-}
-
-dependencies {
-
-    /* Core */
-    implementation("com.thoughtworks.xstream:xstream:1.4.7")
-    implementation("org.slf4j:slf4j-api:1.7.36")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("com.google.guava:guava:31.1-jre")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("com.google.code.gson:gson:2.9.0")
-
-    /* Jackson */
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
-
-    /* Desktop / UI */
-    implementation("com.dorkbox:Notify:3.7")
-    implementation("com.intellij:forms_rt:7.0.3")
-
-    /* Reflection */
-    implementation("net.oneandone.reflections8:reflections8:0.11.7")
-
-    /* Runelite */
-    implementation("net.runelite.pushingpixels:trident:1.5.00")
-    implementation("net.runelite.pushingpixels:substance:8.0.02")
-    implementation("net.runelite:discord:1.1")
-    implementation("com.google.inject:guice:4.2.2")
-    implementation("com.squareup.okhttp3:okhttp:4.3.0")
-
-    /* Apache extras */
-    implementation("org.apache.commons:commons-csv:1.7")
-    implementation("org.apache.commons:commons-text:1.8")
-    implementation("net.sf.jopt-simple:jopt-simple:5.0.4")
-
-    /* Lombok */
-    compileOnly("org.projectlombok:lombok:1.18.8")
-    annotationProcessor("org.projectlombok:lombok:1.18.8")
-
-    /* Testing */
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.register<Jar>("createStandardJar") {
-    archiveFileName.set("NotObfuscatedClient.jar")
-    from(sourceSets["main"].output)
-    manifest {
-        attributes["Main-Class"] = "com.client.Client"
+    named<CreateStartScripts>("startScripts").configure {
+        enabled = false
     }
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get()
-            .filter { it.name.endsWith(".jar") }
-            .map { zipTree(it) }
-    })
-}
-
-
-
-tasks.register<ProGuardTask>("obfuscateStandard") {
-    configuration("proguard.conf")
-
-    configurations.runtimeClasspath.get().forEach {
-        libraryjars(it)
+    named<CreateStartScripts>(SHADOW_SCRIPTS_TASK_NAME).configure {
+        enabled = false
+    }
+    named(SHADOW_INSTALL_TASK_NAME).configure {
+        enabled = false
+    }
+    named("shadowDistTar").configure {
+        enabled = false
+    }
+    named("shadowDistZip").configure {
+        enabled = false
+    }
+    withType<JavaCompile>().configureEach {
+        options.isWarnings = false
+        options.isDeprecation = false
+        options.encoding = "UTF-8"
     }
 
-    injars("build/libs/NotObfuscatedClient.jar")
-    outjars("build/libs/deploy/ObfuscatedClient.jar")
-}
+    register("allatoriJar", JavaExec::class.java) {
+        group = "allatori"
+        mainClass.set("com.allatori.Obfuscate")
+        classpath = files("$rootDir/allatori/allatori.jar")
+        args("$rootDir/allatori.xml")
 
-tasks.register("buildJars") {
-    dependsOn("createStandardJar")
-    dependsOn("obfuscateStandard")
-}
+        jvmArgs("-Xmx2g", "-Xms1g")
 
+        outputs.upToDateWhen { false }
+        dependsOn("shadowJar")
+    }
+    register("allatoriStackTraceRestore", JavaExec::class.java) {
+        group = "allatori"
+        mainClass.set("com.allatori.StackTrace2")
+        classpath = files("$rootDir/allatori/allatori.jar")
+        args("$rootDir/renaming-log.xml", "$rootDir/allatori/input.txt", "$rootDir/allatori/output.txt")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
-rootProject.name = "Xeros Client"
+rootProject.name = "tarnish-client"
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://repo.runelite.net")
+        jcenter()
+        maven("https://jitpack.io")
+        maven("https://repo.spongepowered.org/maven")
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    plugins {
+        id("com.github.johnrengelman.shadow") version "8.1.1"
+    }
+}
